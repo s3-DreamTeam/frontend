e6c269e0d8d11f6b99cbeec119fd78a0b80e2a4d
LyamBRS <cous5830@gmail.com>
2024-10-01 09:22:51 +0000

    test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React, { useEffect, useState } from 'react';
 import Keycloak from 'keycloak-js';
 import { useSelector } from 'react-redux';
 import { setAuthError, setUserToken } from './store/authenticatedSlice';
+import './App.css';
+import { ThemeProvider } from "@mui/material/styles";
+import GetMUIAppTheme from './theme/materialTheme';
+import { CssBaseline } from '@mui/material';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Analytics from './components/pages/Analytics/analytics';
+import MachineManager from './components/pages/Machine Manager/machineManager';
+import StockManager from './components/pages/Stock Manager/stockManager';
+import MachineEditor from './components/pages/Machine Editor/machineEditor';
+import StockEditor from './components/pages/Stock Editor/stockEditor';
+import Changelogs from './components/pages/Changelogs/changelogs';
+import { AppRoutes } from './utils/routerRouteManager';
+import NotLoggedInPage from './components/pages/Not logged in/notLoggedInPage';
+import { KeycloakProvider } from './api/keycloakConfig';
+
 
 const App = () => {
   const token = useSelector((state) => state.authenticated.token);
@@ -39,16 +54,52 @@ const App = () => {
     keycloakInstance.logout();
   };
 
+  const appTheme = GetMUIAppTheme();
+
   if (keycloakInstance) {
     console.log("8");
     if (auth) {
       console.log("9");
       return (
-        <div>
-          <h1>Welcome!</h1>
-          <p>Username: {keycloakInstance.tokenParsed?.preferred_username}</p>
-          <button onClick={logout}>Logout</button>
-        </div>
+        <ThemeProvider theme={appTheme}>
+          <CssBaseline />
+          <Router>
+            <Routes>
+              <Route
+                path={AppRoutes.Analytics}
+                element={<Analytics />}
+              />
+              <Route
+                path={AppRoutes.MachineManager}
+                element={<MachineManager />}
+              />
+              <Route
+                path={AppRoutes.StockManager}
+                element={<StockManager />}
+              />
+              <Route
+                path={AppRoutes.MachineEditor}
+                element={<MachineEditor />}
+              />
+              <Route
+                path={AppRoutes.StockEditor}
+                element={<StockEditor />}
+              />
+              <Route
+                path={AppRoutes.Changelogs}
+                element={<Changelogs />}
+              />
+              <Route
+                path={AppRoutes.NotLoggedIn}
+                element={<NotLoggedInPage />}
+              />
+              <Route
+                path='*'
+                element={<Navigate to={AppRoutes.Home} />}
+              />
+            </Routes>
+          </Router>
+        </ThemeProvider>
       );
     } else {
       console.log("10");
