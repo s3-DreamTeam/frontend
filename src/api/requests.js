3ada3d4fda081c7255c04c67cf93aafeca16b954
LyamBRS <cous5830@gmail.com>
2024-10-04 19:19:58 +0000

    XML?

--- src/api/requests.js
+++ src/api/requests.js
@@ -39,7 +39,7 @@ export const FetchAllUsers = async ({
         const header = BackendHeader();
         const response = await backendApi.get(Endpoints.GetAllUsers, {
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/xml',
                 'Accept': 'application/json'  // Expecting JSON response
             }
         });
