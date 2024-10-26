e2431eda741c7839a1716d0d27534969eff02413
LyamBRS <cous5830@gmail.com>
2024-10-26 00:30:57 +0000

    Fixed missing text in delete loading dialog

--- src/components/pages/InventoryFoundation/mainLayoutFoundation.js
+++ src/components/pages/InventoryFoundation/mainLayoutFoundation.js
@@ -93,7 +93,7 @@ const MainLayoutFoundation = ({
             autoHideDuration: 2000
         },
         loading: {
-            message: deleteLoading,
+            message: "deleting " + selectedName,
             canClickAway: false,
             hasCloseButton: false,
             canRetry: false,
