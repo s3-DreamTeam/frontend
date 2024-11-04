8318471a38fb02479a52c77d5423288dc7642564
LyamBRS <cous5830@gmail.com>
2024-11-04 22:33:58 +0000

    Testing Charle's pipeline

--- src/components/pages/Stock Manager/stockManager.js
+++ src/components/pages/Stock Manager/stockManager.js
@@ -1,3 +1,53 @@
+import { useSelector } from "react-redux";
+import HandleUserLoggedInStatus from "../../../utils/verifyLoggedIn";
+import InventoryFoundationPage from "../InventoryFoundation/foundationPage";
+import { productTemplateFormBuilder } from "../../../utils/formUtils/formBuilderTemplates";
+import ProductTemplateComponentCard from "../../ComponentCards/productTemplateCard";
+import { setProductTemplateToLoaded, setProductTemplateToLoading } from "../../../store/productTemplateSlice";
+import { NewProductTemplate } from "../../../api/requests/interface/ProductTemplates/new";
+import { DeleteProductTemplate } from "../../../api/requests/interface/ProductTemplates/delete";
+import { LoadUsersProductTemplates } from "../../../utils/ComplexStoreManagers/ProductTemplate/load";
+import { UpdateUserProductTemplates } from "../../../utils/ComplexStoreManagers/ProductTemplate/update";
+
+const StockManager = () => {
+    const loadedUserTemplatesBefore = useSelector((state) => state.initialDataLoadStatus.productTemplatesLoaded);
+    const templates = useSelector((state) => state.productTemplateSlice.productTemplates);
+
+    HandleUserLoggedInStatus();
+
+    return (
+        <InventoryFoundationPage
+            MainPageTitle="Stock Manager"
+            emptyInventoryTitle="You don't have any products"
+            emptyInventoryMessage="Ensure you created templates before adding new products to your inventory."
+            loadingSnackbarMessage="Loading products..."
+            deleteObjectDialogMessage="This product will be removed from all machines that currently has it"
+
+            AddNewFormTitle="New Product"
+            addNewSuccessDialogTitle="Product created"
+            addNewSuccessDialogMessage="Your product was successfully created and added to your inventory!"
+            addNewFormObjectGetter={productTemplateFormBuilder}
+
+            ComponentCard={ProductTemplateComponentCard}
+
+            setObjectToLoadedReducer={setProductTemplateToLoaded}
+            setObjectToLoadingReducer={setProductTemplateToLoading}
+
+            alreadyLoadedInventorySelector={loadedUserTemplatesBefore}
+            inventoryObjectsSelector={templates}
+
+            APICreateNewObject={NewProductTemplate}
+            APIDeleteObject={DeleteProductTemplate}
+            LoadInventory={LoadUsersProductTemplates}
+            UpdateInventory={UpdateUserProductTemplates}
+        />
+    );
+};
+
+export default StockManager;
+
+
+/*
 import HandleUserLoggedInStatus from "../../../utils/verifyLoggedIn";
 import EmptyPage from "../../emptyPage";
 import PageLayout from "../../pageLayout/pageLayout";
@@ -21,3 +71,4 @@ const StockManager = () => {
 };
 
 export default StockManager;
+*/
