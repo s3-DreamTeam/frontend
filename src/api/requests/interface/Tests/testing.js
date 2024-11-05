06525a9e20573cfb70cdda022d52d6c26371e85e
LyamBRS <cous5830@gmail.com>
2024-11-05 22:02:09 +0000

    No more header

--- src/api/requests/interface/Tests/testing.js
+++ src/api/requests/interface/Tests/testing.js
@@ -16,7 +16,7 @@ export const FetchBackendTest = async ({
     onStart();
     try {
         const header = BackendHeader();
-        const response = await backendApi.get(Endpoints.Test, header);
+        const response = await backendApi.get(Endpoints.Test);
         console.log("REQ: FetchBackendTest - SUCCESS");
         console.log(response);
         onSuccess(response.data);
