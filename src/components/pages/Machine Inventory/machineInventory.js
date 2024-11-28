58837283d34628d2fe2536be4970a8ac9c02f036
LyamBRS <cous5830@gmail.com>
2024-11-28 02:20:06 +0000

    CHARLE

--- src/components/pages/Machine Inventory/machineInventory.js
+++ src/components/pages/Machine Inventory/machineInventory.js
@@ -448,6 +448,7 @@ const MachineInventoryPage = () => {
     }
 
     // - Retry getting inventory - //
+    // Allo charle - //
     function RetryGettingInventory() {
         GetFullInventory(machineID, true);
     }
